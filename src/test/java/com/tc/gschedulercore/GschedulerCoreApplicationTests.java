530e34b8a861184151ad10441135ea083677177d
honggang.liu <honggang.liu@shopee.com>
2024-09-20 11:08:09 +0800

    init

---
+++ src/test/java/com/tc/gschedulercore/GschedulerCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tc.gschedulercore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GschedulerCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
